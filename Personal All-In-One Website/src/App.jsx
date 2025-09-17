5fed49f73a8abdc93fabbfb02c4d4d0a2bd13500
Riley <dickersonevan82@gmail.com>
2025-09-17 04:07:34 -0400

    added cat

--- Personal All-In-One Website/src/App.jsx
+++ Personal All-In-One Website/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <div id='block'>
           <h1 id='text'>&#127800; lalala &#127800;</h1>
           <text id='text'>there is information here</text>
+          <image src=''></image>
         </div>
       </div>
     </>
