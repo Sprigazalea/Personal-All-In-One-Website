fb72a51a9347040f36203237d0ddbdf25b1b9ec7
Riley <dickersonevan82@gmail.com>
2025-09-18 02:14:09 -0400

    trying to get tailwind working xmx

--- Personal All-In-One Website/src/App.jsx
+++ Personal All-In-One Website/src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div id='main' className='flex justify-normal'>
-        <div id='block'>
-          <h1 id='text'>&#127800; Sprigazalea &#127800;</h1>
-          <text id='text'>there is information here</text>
-          <img src='Personal All-In-One Website/public/474F3583-B516-4547-9537-1DEEB0578ABB.png'></img>
-        </div>
+    <div id='main' className='flex justify-normal content-normal min-h-screen min-w-screen text-black'>
+      <div id='block'>
+        <h1 id='text'>&#127800; Sprigazalea &#127800;</h1>
+        <text id='text'>there is information here</text>
       </div>
-    </>
+    </div>
   )
 }
 
