028ec1d22bd9952ff1ebc513a3ac9d7746da38f7
Riley <dickersonevan82@gmail.com>
2025-09-18 02:56:22 -0400

    tailwind seems to work just not for this specific div

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import './App.css'
 
 function App() {
   return (
-    <div id='main' className='flex justify-normal content-normal min-h-screen min-w-screen text-black'>
-      <div id='block' className=''>
-        <h1 id='text' className='text-4xl'>&#127800; Sprigazalea &#127800;</h1>
-        <text id='text' className='text-3xl'>there is information here</text>
+    <div>
+      <div id='main' className='flex justify-normal content-normal min-h-screen min-w-screen text-black'>
+        <div id='block' className=''>
+          <h1 id='text' className='text-4xl'>&#127800; Sprigazalea &#127800;</h1>
+          <text id='text' className='text-3xl'>there is information here</text>
+        </div>
+        <text className='text-red-900'>test</text>
+        <div className='text-green-900'>test2</div>
       </div>
     </div>
   )
