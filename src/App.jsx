7c6f92711615079fb672102a5cbf445c79d4554e
Riley <dickersonevan82@gmail.com>
2025-09-18 04:30:05 -0400

    added new div and readjusted padding

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,10 @@ function App() {
     <div>
       <div className='flex justify-center items-center min-h-screen min-w-screen text-black'>
         <div className='flex flex-col flex-[0_1_80rem] m-[0px_100px_0px_100px] bg-white min-h-[80vh] rounded-[30px]'>
-          <h1 className='text-4xl text-center pt-[40px]'>&#127800; Sprigazalea &#127800;</h1>
-          <text className='text-xl text-center pt-[20px]'>persephone / riley, but you can call me percy</text>
+          <div className='text-center'>
+            <h1 className='text-4xl font-semibold pt-[40px] pb-[15px]'>&#127800; Sprigazalea &#127800;</h1>
+            <text className='text-xl font-light'>persephone / riley, but you can call me percy</text>
+          </div>
         </div>
       </div>
     </div>
