ea3c21537729bf0d57f94695972d2f21eaaa5563
Riley <dickersonevan82@gmail.com>
2025-09-18 03:19:33 -0400

    removed ids

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import './App.css'
 function App() {
   return (
     <div>
-      <div id='main' className='flex justify-center items-center min-h-screen min-w-screen text-black'>
-        <div id='block' className='flex flex-col bg-white max-h-80vh h-[80vh] w-[70vh] rounded-[30px]'>
-          <h1 id='text' className='text-4xl text-center'>&#127800; Sprigazalea &#127800;</h1>
-          <text id='text' className='text-3xl text-center'>there is information here</text>
+      <div className='flex justify-center items-center min-h-screen min-w-screen text-black'>
+        <div className='flex flex-col bg-white min-h-[80vh] min-w-[70vh] rounded-[30px]'>
+          <h1 className='text-4xl text-center'>&#127800; Sprigazalea &#127800;</h1>
+          <text className='text-2xl text-center'>there is information here</text>
         </div>
       </div>
     </div>
