a9fe8ea20811bc060ec6083640efbc82a2673228
Riley <dickersonevan82@gmail.com>
2025-09-18 04:20:05 -0400

    added padding to text elements

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ function App() {
     <div>
       <div className='flex justify-center items-center min-h-screen min-w-screen text-black'>
         <div className='flex flex-col flex-[0_1_80rem] m-[0px_100px_0px_100px] bg-white min-h-[80vh] rounded-[30px]'>
-          <h1 className='text-4xl text-center'>&#127800; Sprigazalea &#127800;</h1>
-          <text className='text-2xl text-center'>there is information here</text>
+          <h1 className='text-4xl text-center pt-[40px]'>&#127800; Sprigazalea &#127800;</h1>
+          <text className='text-xl text-center pt-[20px]'>persephone / riley, but you can call me percy</text>
         </div>
       </div>
     </div>
