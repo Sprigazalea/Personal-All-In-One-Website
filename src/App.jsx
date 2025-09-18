4fefd58a10d266d6c9a8ca4e53adb25a11c2cc63
Riley <dickersonevan82@gmail.com>
2025-09-18 02:43:06 -0400

    tailwind seems to be working just not in the main div

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
   return (
     <div id='main' className='flex justify-normal content-normal min-h-screen min-w-screen text-black'>
-      <div id='block'>
-        <h1 id='text'>&#127800; Sprigazalea &#127800;</h1>
-        <text id='text'>there is information here</text>
+      <div id='block' className=''>
+        <h1 id='text' className='text-4xl'>&#127800; Sprigazalea &#127800;</h1>
+        <text id='text' className='text-3xl'>there is information here</text>
       </div>
     </div>
   )
