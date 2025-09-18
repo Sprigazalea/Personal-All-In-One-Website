1ed888db1aed4b5edbed1e3827510265152f7d21
Riley <dickersonevan82@gmail.com>
2025-09-18 01:33:19 -0400

    added tailwindcss

--- Personal All-In-One Website/src/App.jsx
+++ Personal All-In-One Website/src/App.jsx
@@ -6,11 +6,11 @@ function App() {
 
   return (
     <>
-      <div id='main'>
+      <div id='main' className='flex justify-normal'>
         <div id='block'>
-          <h1 id='text'>&#127800; lalala &#127800;</h1>
+          <h1 id='text'>&#127800; Sprigazalea &#127800;</h1>
           <text id='text'>there is information here</text>
-          <image src=''></image>
+          <img src='Personal All-In-One Website/public/474F3583-B516-4547-9537-1DEEB0578ABB.png'></img>
         </div>
       </div>
     </>
