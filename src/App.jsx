d12927a1e06d94f00821ab6f478ad837bcde461b
Riley <dickersonevan82@gmail.com>
2025-09-19 00:55:03 -0400

    testing images and reesponsiveness

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,11 @@ function App() {
   return (
     <div>
       <div className='flex justify-center items-center min-h-screen min-w-screen text-black'>
-        <div className='flex flex-col flex-[0_1_80rem] m-[0px_100px_0px_100px] bg-white min-h-[80vh] rounded-[30px]'>
-          <div className='text-center'>
+        <div id='block' className='flex flex-[0_1_80rem] m-[0px_100px_0px_100px] bg-white min-h-[80vh] rounded-[30px]'>
+          <div className='flex flex-col'>
+            <img className='h-[400px]' src='/474F3583-B516-4547-9537-1DEEB0578ABB.png'></img>
+          </div>
+          <div className='flex flex-col flex-[1_0] text-center'>
             <h1 className='text-4xl font-semibold pt-[40px] pb-[15px]'>&#127800; Sprigazalea &#127800;</h1>
             <text className='text-xl font-light'>persephone / riley, but you can call me percy</text>
           </div>
